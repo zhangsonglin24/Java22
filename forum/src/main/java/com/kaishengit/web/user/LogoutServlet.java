dfcab3938b4fd94e31c9c3f2b547a2db289b4bb5
zhangsonglin24 <13183136301@163.com>
2016-12-16 22:14:27 +0800

    做好用户安全退出

---
+++ forum/src/main/java/com/kaishengit/web/user/LogoutServlet.java
@@ -0,0 +1,21 @@
+package com.kaishengit.web.user;
+
+import com.kaishengit.web.BaseServlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebServlet("/logout")
+public class LogoutServlet extends BaseServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession();
+        session.invalidate();
+
+        resp.sendRedirect("/login?state=logout");
+    }
+}
